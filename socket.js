c071e53c7cb0e7a8c845c20e5fd288c8f9bc1e21
andreek <andreek@ccd0ef04-3c04-447b-9bd2-a41a160fc93b>
2011-07-10 21:45:24 +0000

    #167 broadcasting new regions/kinects/commands
    git-svn-id: https://nyx.informatik.uni-bremen.de/svn/SmartEnergy/groups/kinect/src/server/branches/0.3@712 ccd0ef04-3c04-447b-9bd2-a41a160fc93b

--- socket.js
+++ socket.js
@@ -21,31 +21,47 @@ module.exports.configureSocket = function(app) {
     });
 
     socket.on('newRegion', function(region) {
+
       if(app.regions.isValid(region)){
         app.regions.push(region);      
         socket.broadcast.emit('newRegion', region);  
       }
+
     });
     
+    socket.on('updateRegion', function(region) {
+
+      if(app.regions.isValid(region)){
+        app.regions.push(region);      
+        socket.broadcast.emit('updateRegion', region);  
+      }
+
+    });
     socket.on('removeRegion', function(key) {
+
       if(key){
         app.regions.remove(key); 
         socket.broadcast.emit('removedRegion', key);   
       }
+
     });
 
     socket.on('newCommand', function(command) {
+
       if(app.commands.isValid(command)){
         app.commands.push(command);      
         socket.broadcast.emit('newCommand', command);  
       }
+
     });
 
     socket.on('removeCommand', function(key) {
+
       if(key){
         app.commands.remove(key);
         socket.broadcast.emit('removedCommand', key);   
       }
+
     });
 
   });
@@ -94,60 +110,3 @@ function defineUserMsgs(app, sockets) {
   });
 
 };
-
-/**
- * all socket events for kinects
- */
-function defineKinectMsgs(app, sockets) {
-  
-  app.kinects.addListener('new', function(key, kinect) {
-    sockets.emit('newKinect', kinect);  
-  });
-  
-  app.kinects.addListener('update', function(key, kinect) {
-    sockets.emit('updateKinect', kinect);  
-  });
-  
-  app.kinects.addListener('removed', function(key) {
-    sockets.emit('removedKinect', key);   
-  });
-
-}
-
-/**
- * all socket events for regions
- */
-function defineRegionMsgs(app, socket, sockets) {
-  
-  app.regions.addListener('new', function(key, region) {
-    sockets.emit('newRegion', region);  
-  });
-  
-  app.regions.addListener('update', function(key, region) {
-    sockets.emit('updateRegion', region);  
-  });
-  
-  app.regions.addListener('removed', function(key) {
-    sockets.emit('removedRegion', key);    
-  });
-
-}
-
-/**
- * all socket events for commands
- */
-function defineCommandMsgs(app, socket, sockets) {
-
-  app.commands.addListener('new', function(key, command) {
-    sockets.emit('newCommand', command);  
-  });
-  
-  app.commands.addListener('update', function(key, command) {
-    sockets.emit('updateCommand', command);  
-  });
-  
-  app.commands.addListener('removed', function(key) {
-    sockets.emit('removedCommand', key);     
-  });
-
-}
